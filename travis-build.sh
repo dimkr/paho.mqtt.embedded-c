b8c5ff8933ca5435fe0b22789675367225080449
Dima Krasner <dima@dimakrasner.com>
2020-04-18 11:41:37 +0300

    Build with optimization and test with ASan

--- travis-build.sh
+++ travis-build.sh
@@ -3,11 +3,13 @@
 set -e
 
 rm -rf build.paho
-meson --werror build.paho
+meson -Doptimization=3 -Ddebug=true --werror build.paho
 cd build.paho
 echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
 ninja
 python ../test/mqttsas2.py localhost 1883 1885 &
+meson test -v --no-rebuild
+meson configure -Db_sanitize=address
 meson test -v
 kill %1
 killall mosquitto
