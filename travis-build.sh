bcb0d2caea0258234a2df388dd36e890f4122f7e
Dima Krasner <dima@dimakrasner.com>
2020-04-18 12:22:02 +0300

    Run tests only once on Travis

--- travis-build.sh
+++ travis-build.sh
@@ -3,12 +3,10 @@
 set -e
 
 rm -rf build.paho
-meson -Doptimization=3 -Ddebug=true --werror build.paho
+meson -Doptimization=3 -Ddebug=true --werror -Db_sanitize=address build.paho
 cd build.paho
 echo "travis build dir $TRAVIS_BUILD_DIR pwd $PWD"
 ninja
 python ../test/mqttsas2.py localhost 1883 1885 &
 meson test -v --no-rebuild
-meson configure -Db_sanitize=address
-meson test -v
 kill %1
