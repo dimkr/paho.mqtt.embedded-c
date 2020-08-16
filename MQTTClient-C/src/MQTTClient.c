34779f84d915d234ea1308cbe4ba3738be2baba0
Dima Krasner <dima@dimakrasner.com>
2020-08-16 22:51:17 +0300

    Use one timer for last I/O time

--- MQTTClient-C/src/MQTTClient.c
+++ MQTTClient-C/src/MQTTClient.c
@@ -45,7 +45,7 @@ static int sendPacket(MQTTClient* c, int length, Timer* timer)
     }
     if (sent == length)
     {
-        TimerCountdown(&c->last_sent, c->keepAliveInterval); // record the fact that we have successfully sent the packet
+        TimerCountdown(&c->last_io, c->keepAliveInterval); // record the fact that we have successfully sent the packet
         rc = SUCCESS;
     }
     else
@@ -72,8 +72,7 @@ void MQTTClientInit(MQTTClient* c, Network* network, unsigned int command_timeou
     c->ping_outstanding = 0;
     c->defaultMessageHandler = NULL;
 	  c->next_packetid = 1;
-    TimerInit(&c->last_sent);
-    TimerInit(&c->last_received);
+    TimerInit(&c->last_io);
 #if defined(MQTT_TASK)
 	  MutexInit(&c->mutex);
 #endif
@@ -139,7 +138,7 @@ static int readPacket(MQTTClient* c, Timer* timer)
     header.byte = c->readbuf[0];
     rc = header.bits.type;
     if (c->keepAliveInterval > 0)
-        TimerCountdown(&c->last_received, c->keepAliveInterval); // record the fact that we have successfully received a packet
+        TimerCountdown(&c->last_io, c->keepAliveInterval); // record the fact that we have successfully received a packet
 exit:
     return rc;
 }
@@ -216,14 +215,13 @@ int MQTTKeepalive(MQTTClient* c, int timeout_ms)
     if (c->keepAliveInterval == 0)
         goto exit;
 
-    if (TimerIsExpired(&c->last_sent) || TimerIsExpired(&c->last_received))
+    if (TimerIsExpired(&c->last_io))
     {
         if (c->ipstack->mqttkeepalive)
         {
             if (c->ipstack->mqttkeepalive(c->ipstack, timeout_ms, c->keepAliveInterval) <= 0)
                 rc = FAILURE;
-            TimerCountdown(&c->last_sent, c->keepAliveInterval);
-            TimerCountdown(&c->last_received, c->keepAliveInterval);
+            TimerCountdown(&c->last_io, c->keepAliveInterval);
             goto exit;
         }
 
@@ -440,7 +438,7 @@ int MQTTConnectWithResults(MQTTClient* c, MQTTPacket_connectData* options, MQTTC
 
     c->keepAliveInterval = options->keepAliveInterval;
     c->cleansession = options->cleansession;
-    TimerCountdown(&c->last_received, c->keepAliveInterval);
+    TimerCountdown(&c->last_io, c->keepAliveInterval);
     if ((len = MQTTSerialize_connect(c->buf, c->buf_size, options)) <= 0)
         goto exit;
     if ((rc = sendPacket(c, len, &connect_timer)) != SUCCESS)  // send the connect packet
